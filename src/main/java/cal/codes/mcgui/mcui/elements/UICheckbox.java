1437f4cacf11e344a6de19dab021ad4933861d48
CyaCal <calumproh28@gmail.com>
2021-01-01 15:03:19 +0000

    CheckboxRenderer and UICheckbox class complete. Now for parser.

--- src/main/java/cal/codes/mcgui/mcui/elements/UICheckbox.java
+++ src/main/java/cal/codes/mcgui/mcui/elements/UICheckbox.java
@@ -1,7 +1,21 @@
 package cal.codes.mcgui.mcui.elements;
 
+import net.minecraft.text.Text;
+
 public class UICheckbox extends UIElement {
     public UICheckbox() {
         type = UIType.CHECKBOX;
     }
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public Text contents;
+
+    public boolean defaultState;
+
+    public String changeState;
+
 }
--- src/main/java/cal/codes/mcgui/mcui/renderers/CheckboxRenderer.java
+++ src/main/java/cal/codes/mcgui/mcui/renderers/CheckboxRenderer.java
@@ -1,11 +1,35 @@
 package cal.codes.mcgui.mcui.renderers;
 
+import cal.codes.mcgui.exceptions.RegistryNotFoundException;
+import cal.codes.mcgui.logging.Logger;
+import cal.codes.mcgui.mcui.MethodsRegistry;
 import cal.codes.mcgui.mcui.elements.UICheckbox;
 import cal.codes.mcgui.mcui.elements.UIDocument;
+import cal.codes.mcgui.mcui.events.CheckboxEventArgs;
+import me.lambdaurora.spruceui.Position;
+import me.lambdaurora.spruceui.widget.SpruceCheckboxWidget;
 
 public class CheckboxRenderer implements Renderer<UICheckbox> {
     @Override
     public void render(UIDocument document, UICheckbox element) {
+        SpruceCheckboxWidget tmp = new SpruceCheckboxWidget(
+                Position.of(element.x, element.y),
+                element.width,
+                element.height,
+                element.contents,
+                (button, newValue) -> {
+                    try {
+                        MethodsRegistry.fetch(element.changeState).invoke(element, document, new CheckboxEventArgs(newValue));
+                    } catch (RegistryNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                },
+                element.defaultState
+        );
 
+        tmp.setVisible(true);
+
+        document.add(tmp);
+        Logger.info("Registered checkbox with action.");
     }
 }
